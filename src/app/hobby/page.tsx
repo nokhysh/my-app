5990eaf9efec3088cd7dbf2f8af3af0bf634120a
nokhysh <77913984+nokhysh@users.noreply.github.com>
2024-04-06 17:00:15 +0900

    趣味ページの作成

--- src/app/hobby/page.tsx
+++ src/app/hobby/page.tsx
@@ -1,12 +1,34 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Hobby = () => {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
-      <div className="">My Hobby</div>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-yellow-100 gap-5">
+      <div className="text-5xl underline underline-offset-[12px]">My Hobby</div>
+      <div className="flex gap-5">
+        <div className="w-52 rounded overflow-hidden shadow-lg bg-green-300">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">漫画&アニメ</div>
+            <div className="text-gray-700 text-base">
+              <li>NARUTO</li>
+              <li>ONE PEACE</li>
+              <li>呪術廻戦</li>
+            </div>
+          </div>
+        </div>
+        <div className="w-52 rounded overflow-hidden shadow-lg bg-orange-300">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">運動</div>
+            <div className="text-gray-700 text-base">
+              <li>筋トレ</li>
+              <li>フットサル</li>
+              <li>ランニング</li>
+            </div>
+          </div>
+        </div>
+      </div>
       <Link href="/">Homeへ</Link>
     </div>
   );
-}
+};
 
 export default Hobby;
